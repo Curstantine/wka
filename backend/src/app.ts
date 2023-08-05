06fdac0700e9ec1928c3b8ae534a2aeec5c8d167
Curstantine <resistlyfe@gmail.com>
2023-08-05 13:47:13 +0530

    fix: setup headers in server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,6 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use((_, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	res.header("Access-Control-Allow-Methods", "GET, POST");
+	next();
+});
+
 app.use("/", rootRouter);
 app.use("/auth", authRouter);
 app.use("/form", formRouter);
