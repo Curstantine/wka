3b3629380b2525e252f12185014a4966bf746e0b
Curstantine <resistlyfe@gmail.com>
2023-08-10 19:10:25 +0530

    chore: refactor imports and components

---
+++ src/components/nav.tsx
@@ -0,0 +1,84 @@
+import { animated, useSpring } from "@react-spring/web";
+import clsx from "clsx";
+
+export default function NavigationShell() {
+	const destinations = [
+		"Home",
+		"About",
+		"KRKNomics",
+		"Roadmap",
+		"Team",
+		"Ring of KRKN",
+		"Articles",
+		"Join Us",
+	];
+
+	return (
+		<div className="flex items-center mx-4">
+			<img src="/logo.png" alt="logo" className="w-12 h-12" />
+			<div className="flex-1 inline-flex justify-end gap-6">
+				{destinations.map((destination) => (
+					<a
+						href="/"
+						className={clsx(
+							"font-rubik font-bold text-lg h-fit uppercase",
+							"border-b-6 border-b-solid border-b-white",
+						)}
+					>
+						{destination}
+					</a>
+				))}
+				<WeirdAhhNavigationExpandButton />
+			</div>
+		</div>
+	);
+}
+
+const WeirdAhhNavigationExpandButton = () => {
+	const [props, set] = useSpring(() => ({
+		from: { x: 0 },
+		to: { x: 1 },
+		config: { duration: 300, easing: (t) => t * (2 - t) },
+	}));
+
+	const onEnter = () => set({ x: 1 });
+	const onLeave = () => set({ x: 0 });
+
+	return (
+		<button
+			onMouseEnter={onEnter}
+			onMouseLeave={onLeave}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				xmlnsXlink="http://www.w3.org/1999/xlink"
+				width="47.25"
+				height="35"
+				viewBox="0 0 47.25 35"
+			>
+				<animated.path
+					style={{
+						transformOrigin: "center",
+						translateX: props.x.to({
+							range: [0, 1],
+							output: [0, -10],
+						}),
+					}}
+					d="M1.25 35a1.176 1.176 0 0 1-.9-.375A1.242 1.242 0 0 1 0 33.75V1.25a1.218 1.218 0 0 1 .35-.9 1.218 1.218 0 0 1 .9-.35H24.6a1.218 1.218 0 0 1 .9.35 1.218 1.218 0 0 1 .35.9v4.7a1.218 1.218 0 0 1-.35.9 1.218 1.218 0 0 1-.9.35H8.65v6.75H23.5a1.176 1.176 0 0 1 .9.375 1.3 1.3 0 0 1 .35.925v4.35a1.242 1.242 0 0 1-.35.875 1.176 1.176 0 0 1-.9.375H8.65v6.95H25a1.218 1.218 0 0 1 .9.35 1.218 1.218 0 0 1 .35.9v4.7a1.242 1.242 0 0 1-.35.875A1.176 1.176 0 0 1 25 35Z"
+				>
+				</animated.path>
+				<animated.path
+					style={{
+						transformOrigin: "center",
+						translateX: props.x.to({
+							range: [0, 1],
+							output: [0, 10],
+						}),
+					}}
+					d="M46 35a1.176 1.176 0 0 0 .9-.375 1.242 1.242 0 0 0 .35-.875V1.25a1.218 1.218 0 0 0-.35-.9A1.218 1.218 0 0 0 46 0H22.65a1.218 1.218 0 0 0-.9.35 1.218 1.218 0 0 0-.35.9v4.7a1.218 1.218 0 0 0 .35.9 1.218 1.218 0 0 0 .9.35H38.6v6.75H23.75a1.176 1.176 0 0 0-.9.375 1.3 1.3 0 0 0-.35.925v4.35a1.242 1.242 0 0 0 .35.875 1.176 1.176 0 0 0 .9.375H38.6v6.95H22.25a1.218 1.218 0 0 0-.9.35 1.218 1.218 0 0 0-.35.9v4.7a1.242 1.242 0 0 0 .35.875 1.176 1.176 0 0 0 .9.375Z"
+				>
+				</animated.path>
+			</svg>
+		</button>
+	);
+};
--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -2,6 +2,33 @@ import { animated, useSpring } from "@react-spring/web";
 import clsx from "clsx";
 
 import KrknHeroBG from "../assets/Hero_BG_main1.png";
+import NavigationShell from "../components/nav";
+
+export default function Hero() {
+	return (
+		<div className="flex flex-col items-center justify-center h-[100vh] p-24">
+			<div className="relative w-full h-full">
+				<NavigationShell />
+				<div className="flex items-end h-full mx-8 pb-6">
+					<div className="flex flex-col w-full">
+						<h1 className="mb-2 text-5xl tracking-wide font-rubik font-bold tracking-wider uppercase">
+							We are KRKN
+						</h1>
+						<span className="mb-6">The new era in Ethereum</span>
+						<Button />
+					</div>
+					<div className="inline-flex">
+						<Arrow />
+						<Arrow reversed />
+					</div>
+				</div>
+				<div className="absolute inset-0 -z-1">
+					<img src={KrknHeroBG} alt="KrknHeroBG" className="h-full w-full object-cover" />
+				</div>
+			</div>
+		</div>
+	);
+}
 
 const Button = () => {
 	const [props, set] = useSpring(() => ({
@@ -86,111 +113,3 @@ const Arrow = ({ reversed = false }) => {
 		</animated.button>
 	);
 };
-
-const WeirdAhhNavigationExpandButton = () => {
-	const [props, set] = useSpring(() => ({
-		from: { x: 0 },
-		to: { x: 1 },
-		config: { duration: 300, easing: (t) => t * (2 - t) },
-	}));
-
-	const onEnter = () => set({ x: 1 });
-	const onLeave = () => set({ x: 0 });
-
-	return (
-		<button
-			onMouseEnter={onEnter}
-			onMouseLeave={onLeave}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				xmlnsXlink="http://www.w3.org/1999/xlink"
-				width="47.25"
-				height="35"
-				viewBox="0 0 47.25 35"
-			>
-				<animated.path
-					style={{
-						transformOrigin: "center",
-						translateX: props.x.to({
-							range: [0, 1],
-							output: [0, -10],
-						}),
-					}}
-					d="M1.25 35a1.176 1.176 0 0 1-.9-.375A1.242 1.242 0 0 1 0 33.75V1.25a1.218 1.218 0 0 1 .35-.9 1.218 1.218 0 0 1 .9-.35H24.6a1.218 1.218 0 0 1 .9.35 1.218 1.218 0 0 1 .35.9v4.7a1.218 1.218 0 0 1-.35.9 1.218 1.218 0 0 1-.9.35H8.65v6.75H23.5a1.176 1.176 0 0 1 .9.375 1.3 1.3 0 0 1 .35.925v4.35a1.242 1.242 0 0 1-.35.875 1.176 1.176 0 0 1-.9.375H8.65v6.95H25a1.218 1.218 0 0 1 .9.35 1.218 1.218 0 0 1 .35.9v4.7a1.242 1.242 0 0 1-.35.875A1.176 1.176 0 0 1 25 35Z"
-				>
-				</animated.path>
-				<animated.path
-					style={{
-						transformOrigin: "center",
-						translateX: props.x.to({
-							range: [0, 1],
-							output: [0, 10],
-						}),
-					}}
-					d="M46 35a1.176 1.176 0 0 0 .9-.375 1.242 1.242 0 0 0 .35-.875V1.25a1.218 1.218 0 0 0-.35-.9A1.218 1.218 0 0 0 46 0H22.65a1.218 1.218 0 0 0-.9.35 1.218 1.218 0 0 0-.35.9v4.7a1.218 1.218 0 0 0 .35.9 1.218 1.218 0 0 0 .9.35H38.6v6.75H23.75a1.176 1.176 0 0 0-.9.375 1.3 1.3 0 0 0-.35.925v4.35a1.242 1.242 0 0 0 .35.875 1.176 1.176 0 0 0 .9.375H38.6v6.95H22.25a1.218 1.218 0 0 0-.9.35 1.218 1.218 0 0 0-.35.9v4.7a1.242 1.242 0 0 0 .35.875 1.176 1.176 0 0 0 .9.375Z"
-				>
-				</animated.path>
-			</svg>
-		</button>
-	);
-};
-
-const NavigationShell = () => {
-	const destinations = [
-		"Home",
-		"About",
-		"KRKNomics",
-		"Roadmap",
-		"Team",
-		"Ring of KRKN",
-		"Articles",
-		"Join Us",
-	];
-
-	return (
-		<div className="flex items-center mx-4">
-			<img src="/logo.png" alt="logo" className="w-12 h-12" />
-			<div className="flex-1 inline-flex justify-end gap-6">
-				{destinations.map((destination) => (
-					<a
-						href="/"
-						className={clsx(
-							"font-rubik font-bold text-lg h-fit uppercase",
-							"border-b-6 border-b-solid border-b-white",
-						)}
-					>
-						{destination}
-					</a>
-				))}
-				<WeirdAhhNavigationExpandButton />
-			</div>
-		</div>
-	);
-};
-
-export default function Hero() {
-	return (
-		<div className="flex flex-col items-center justify-center h-[100vh] p-24">
-			<div className="relative w-full h-full">
-				<NavigationShell />
-				<div className="flex items-end h-full mx-8 pb-6">
-					<div className="flex flex-col w-full">
-						<h1 className="mb-2 text-5xl tracking-wide font-rubik font-bold tracking-wider uppercase">
-							We are KRKN
-						</h1>
-						<span className="mb-6">The new era in Ethereum</span>
-						<Button />
-					</div>
-					<div className="inline-flex">
-						<Arrow />
-						<Arrow reversed />
-					</div>
-				</div>
-				<div className="absolute inset-0 -z-1">
-					<img src={KrknHeroBG} alt="KrknHeroBG" className="h-full w-full object-cover" />
-				</div>
-			</div>
-		</div>
-	);
-}
