76d5519b2129abbfdff440726497d3df2935ad78
Curstantine <resistlyfe@gmail.com>
2023-08-10 12:05:22 +0530

    init

---
+++ unocss.config.ts
@@ -0,0 +1,22 @@
+// uno.config.ts
+import { defineConfig, presetWebFonts } from "unocss";
+import type { Theme } from "unocss/preset-uno";
+
+export default defineConfig<Theme>({
+	presets: [
+		presetWebFonts({
+			fonts: {
+				"mono": ["Space Mono", "monospace"],
+			},
+		}),
+	],
+	theme: {
+		easing: {
+			emphasized: "cubic-bezier(0.4, 0.0, 0.2, 1.0)",
+			standard: "cubic-bezier(0.2, 0.0, 0, 1.0)",
+		},
+		colors: {
+			accent: "#6dc281",
+		},
+	},
+});
